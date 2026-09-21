5f1ce015482cdd56ea2e168da6e9c94d14e52057
Ali-Armani <armani.ali@proton.me>
2026-09-21 18:14:16 +0330

    feat: add exercise to Student.jsx

--- src/pages/Student/Student.jsx
+++ src/pages/Student/Student.jsx
@@ -1,11 +1,29 @@
 
-function Student() {
+import PropTypes from 'prop-types';
+
+function Student({ name = 'Guest', age = '0', isStudent = 'false' }) {
 
   return(
-    <>
-      A
-    </>
+    <div className='student-container'>
+      <p>
+        Name: {name}
+      </p>
+
+      <p>
+        Age: {age}
+      </p>
+
+      <p>
+        Education Status: {isStudent ? "Student" : "Not a Studen"}
+      </p>
+    </div>
   );
+}
+
+Student.PropTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  isStudent: PropTypes.bool,
 }
 
 export default Student;
