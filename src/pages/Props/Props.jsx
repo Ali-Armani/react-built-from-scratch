dfeb684fce13b8575e92624094a3ba5db5db4acd
Ali-Armani <armani.ali@proton.me>
2026-09-22 14:22:13 +0330

    feat: add prop-types & default props exercise

--- src/pages/Props/Props.jsx
+++ src/pages/Props/Props.jsx
@@ -1,11 +1,35 @@
 
-function Props() {
+import PropTypes from "prop-types";
+
+function Props({name='Guest User', age='0', field='Not mentioned', experience='0', citizenship='Not mentioned'}) {
 
   return(
     <>
-      Props
+      <p>
+        Full Name: {name}
+      </p>
+      <p>
+        Age: {age}
+      </p>
+      <p>
+        Field of Study: {field}
+      </p>
+      <p>
+        Years of Experience: {experience}
+      </p>
+      <p>
+        Citizenship: {citizenship}
+      </p>
     </>
   );
+}
+
+Props.PropTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  field: PropTypes.string,
+  experience: PropTypes.number,
+  citizenship: PropTypes.string,
 }
 
 export default Props;
--- src/pages/Props/PropsPage.jsx
+++ src/pages/Props/PropsPage.jsx
@@ -4,7 +4,17 @@ import Props from './Props.jsx';
 function PropsPage() {
 
   return(
-    <Props />
+    <>
+      <Props ></Props>
+      <hr />
+      <Props name='Peter Parker' age={29} field='Spiderman' experience={1} citizenship='American'></Props>
+      <hr />
+      <Props name='Ali Armani' age={29} field='Web Development' experience={1} citizenship='Iranian'></Props>
+      <hr />
+      <Props field='MBA' experience={1}></Props>
+      <hr />
+      <Props name={20} age='age' field={3} experience='experience' citizenship={1}></Props>
+    </>
   );
 }
 
