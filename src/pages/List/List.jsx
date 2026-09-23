02f0c122576a7073f57cb820b306c4cf083946e3
Ali-Armani <armani.ali@proton.me>
2026-09-23 18:59:39 +0330

    feat: add listItems, li and ol to ListPage

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,10 +1,12 @@
 
 function List() {
 
+  const fruits = [{name: 'Peaches'}, {name: 'Apples'}, {name: 'Plumbs'}, {name: 'Tomatoes'}, {name: 'Bananas'}, {name: 'Oranges'}];
+
+  const listItems = fruits.map(Fruit => <li>{Fruit.name}</li>);
+
   return(
-    <>
-      <p>A</p>
-    </>
+    <><ol>{listItems}</ol></>
   );
 }
 
