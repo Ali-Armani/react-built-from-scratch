f7c496be0bffc7f96659243e4d4f4d2b464378d4
Ali-Armani <armani.ali@proton.me>
2026-09-23 19:07:05 +0330

    feat: add ID and calories to fruits

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,9 +1,16 @@
 
 function List() {
 
-  const fruits = [{name: 'Peaches'}, {name: 'Apples'}, {name: 'Plumbs'}, {name: 'Tomatoes'}, {name: 'Bananas'}, {name: 'Oranges'}];
+  const fruits = [{id: 1, name: 'Peaches', calories: '34'},
+                  {id: 2, name: 'Apples', calories: '107'}, 
+                  {id: 3, name: 'Plumbs', calories: '221'}, 
+                  {id: 4, name: 'Tomatoes', calories: '46'}, 
+                  {id: 5, name: 'Bananas', calories: '53'}, 
+                  {id: 6, name: 'Oranges', calories: '100'}];
 
-  const listItems = fruits.map(Fruit => <li>{Fruit.name}</li>);
+  const listItems = fruits.map(Fruit => <li key = {Fruit.id}> 
+                                                  {Fruit.name}: {Fruit.calories}
+                                                  </li>);
 
   return(
     <><ol>{listItems}</ol></>
