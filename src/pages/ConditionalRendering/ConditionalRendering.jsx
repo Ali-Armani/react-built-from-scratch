5f9a1a25b6c57ce0e5ad6e527747b1cc81e1e4b0
Ali-Armani <armani.ali@proton.me>
2026-09-23 12:02:43 +0330

    feat: add IF Statement

--- src/pages/ConditionalRendering/ConditionalRendering.jsx
+++ src/pages/ConditionalRendering/ConditionalRendering.jsx
@@ -1,11 +1,22 @@
 
+import PropTypes from "prop-types";
+
 function ConditionalRendering() {
 
   return(
-    <>
-      Conditional Rendering
-    </>
+    <div>
+      if (isLoggedIn=true) {
+        <h2>Welcome to our Company.</h2>
+      }
+      else {
+        <h2>Please Log in.</h2>
+      }
+    </div>
   );
+}
+
+ConditionalRendering.PropTypes = {
+  isLoggedIn: true,
 }
 
 export default ConditionalRendering;
